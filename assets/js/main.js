66fb564a6c98ebc661b5aaf601b4571afa12757d
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-07 14:16:38 +0100

    Ask username + make it "let" and show on console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-alert("ciao");
+
+// Chiedi all'utente il suo nome
+let userName = prompt("qual'è il tuo Nome?")
+console.log(userName);
+// poi chiedi il suo cognome
+
+// poi chiedi il suo colore preferito
+
+// Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
