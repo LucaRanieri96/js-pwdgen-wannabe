6df4b4e487ccee612d67465bb4768749d5672264
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-07 15:32:59 +0100

    Add ; after all commands

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 
 // Chiedi all'utente il suo nome
-const userName = prompt("qual'è il tuo Nome?")
+const userName = prompt("qual'è il tuo Nome?");
 console.log(userName);
 
 // poi chiedi il suo cognome
-const userSurname = prompt("qual'è il tuo Cognome?")
+const userSurname = prompt("qual'è il tuo Cognome?");
 console.log(userSurname);
 
 // poi chiedi il suo colore preferito
-const userFavColor = prompt("qual'è il tuo Colore preferito?")
+const userFavColor = prompt("qual'è il tuo Colore preferito?");
 console.log(userFavColor);
 
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
-const password = document.getElementById("yourpassword")
+const password = document.getElementById("yourpassword");
 
-password.innerHTML = `${userName}${userSurname}${userFavColor}23`
+password.innerHTML = `${userName}${userSurname}${userFavColor}23`;
