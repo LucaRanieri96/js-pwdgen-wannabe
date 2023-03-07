dc78cc1d49fb2e463b050a0dda90c14a9aa94c1f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-07 14:17:52 +0100

    Ask surname + make it "let" and show on console

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@
 let userName = prompt("qual'è il tuo Nome?")
 console.log(userName);
 // poi chiedi il suo cognome
-
+let userSurname = prompt("qual'è il tuo Cognome?")
+console.log(userSurname);
 // poi chiedi il suo colore preferito
 
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
