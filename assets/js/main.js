276527fae130d4021a6aebde232b89f39b514945
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-07 14:41:33 +0100

    Write on site password result

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,16 @@
 // Chiedi all'utente il suo nome
 let userName = prompt("qual'è il tuo Nome?")
 console.log(userName);
+
 // poi chiedi il suo cognome
 let userSurname = prompt("qual'è il tuo Cognome?")
 console.log(userSurname);
+
 // poi chiedi il suo colore preferito
 let userFavColor = prompt("qual'è il tuo Colore preferito?")
 console.log(userFavColor);
+
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+const password = document.getElementById("yourpassword")
+
+password.innerHTML = `${userName}${userSurname}${userFavColor}23`
